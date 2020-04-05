4ea883e068dbf5ef55c3712202f3c00fe29cc484
daisuke_watanabe2 <daisuke_watanabe2@icloud.com>
2020-04-06 05:39:18 +0900

    htmlページを実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   get 'users/show' => 'users#show'
   root  'strolls#chiba'
-  get 'strolls/choices' => 'strolls#choices'
+  get 'strolls/ganbarou' => 'strolls#ganbarou'
+  get 'strolls/people' => 'strolls#people'
   resources :strolls
   resources :favorites, only: [:create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update] do
