19e4b07ca9e38e3c893ce152eb814972459838e7
daisuke_watanabe2 <daisuke_watanabe2@icloud.com>
2020-04-05 14:42:07 +0900

    お気に入り一覧を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def show
     @user = current_user
   end
+  def favorites
+    @user = current_user
+    @favorites = Favorite.where(user_id: @user.id).all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  # get 'users/show'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   get 'users/show' => 'users#show'
   root  'strolls#index'
   resources :strolls
   resources :favorites, only: [:create, :destroy]
+  resources :users, only: [:new, :create, :show, :edit, :update] do
+    member do
+      get :favorites
+    end
+  end
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
