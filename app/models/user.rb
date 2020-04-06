98af411ed69ef2057c92db9e7ae896a4459226c7
daisuke_watanabe2 <daisuke_watanabe2@icloud.com>
2020-04-06 22:29:13 +0900

    パスワード変更機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
+  def update_without_current_password(params)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params)
+    clean_up_passwords
+    result
+  end
   has_many :strolls
   has_many :favorites, dependent: :destroy
   validates :name, presence: true
