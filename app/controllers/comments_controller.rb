00aa2bef11c18692e29aaf1db9fab5dd3d020a9e
daisuke_watanabe2 <daisuke_watanabe2@icloud.com>
2020-04-09 04:50:36 +0900

    コメント削除機能を実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,6 +11,18 @@ class CommentsController < ApplicationController
       end
     end
   end
+  def destroy
+    @stroll = Stroll.find(params[:stroll_id])
+    @comment = @stroll.comments.find(params[:id])
+    respond_to do |format|
+      if current_user.id == @comment.user_id
+        @comment.destroy
+        format.js { render :index }
+      else
+        format.html { redirect_to stroll_path(@stroll), notice: '権限がありません。' }
+      end
+    end
+  end
 
   private
 
