5d4e45701b9ee90c5f51ef25a703cb62921f1e14
daisuke_watanabe2 <daisuke_watanabe2@icloud.com>
2020-04-08 21:48:16 +0900

    プロフィール画像がnilのときIMGを表示するよう実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_080641) do
+ActiveRecord::Schema.define(version: 2020_04_08_120141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,10 +59,10 @@ ActiveRecord::Schema.define(version: 2020_04_08_080641) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
-    t.boolean "admin"
     t.text "profile"
     t.text "profile_image"
+    t.string "name"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
