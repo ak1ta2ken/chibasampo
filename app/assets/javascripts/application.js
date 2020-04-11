f8ab35a2924a84ee4815627ca725ebeaf0244698
daisuke_watanabe2 <daisuke_watanabe2@icloud.com>
2020-04-11 21:33:43 +0900

    Herokuへデプロイ

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,11 +11,11 @@
 // about supported directives.
 //
 
+//= require jquery/dist/jquery.js
+//= require bootstrap/dist/js/bootstrap.min
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require jquery/dist/jquery.js
-//= require bootstrap/dist/js/bootstrap.min
 
 (function() {
   const sceneTop = document.getElementById("sceneTop");
