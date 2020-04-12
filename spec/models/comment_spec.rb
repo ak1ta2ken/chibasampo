49c294ebf61f2ceecaf3b6e465c30217fb7c6f38
daisuke_watanabe2 <daisuke_watanabe2@icloud.com>
2020-04-12 21:54:04 +0900

    コメント機能のモデルテストを追加

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @stroll = FactoryBot.create(:stroll, user_id: @user.id)
+  end
+  it "コメントが空ならバリデーションが通らない" do
+    comment = Comment.new(content: "", user_id: @user.id, stroll_id: @stroll.id)
+    expect(comment).not_to be_valid
+  end
+  it "コメントが入力されてたらバリデーションが通る" do
+    comment = Comment.new(content: "chiba", user_id: @user.id, stroll_id: @stroll.id)
+    expect(comment).to be_valid
+  end
 end
