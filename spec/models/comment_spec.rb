a93cf2806ed7878b729c03a7444e781cd0df38d8
daisuke_watanabe2 <daisuke_watanabe2@icloud.com>
2020-04-12 22:34:34 +0900

    コメント機能のテストを修正

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   before do
     @user = FactoryBot.create(:user)
+    @second_user = FactoryBot.create(:second_user)
     @stroll = FactoryBot.create(:stroll, user_id: @user.id)
   end
   it "コメントが空ならバリデーションが通らない" do
-    comment = Comment.new(content: "", user_id: @user.id, stroll_id: @stroll.id)
+    comment = Comment.new(content: "", user_id: @second_user.id, stroll_id: @stroll.id)
     expect(comment).not_to be_valid
   end
   it "user_idが空ならバリデーションが通らない" do
@@ -14,11 +15,11 @@ RSpec.describe Comment, type: :model do
     expect(comment).not_to be_valid
   end
   it "stroll_idが空ならバリデーションが通らない" do
-    comment = Comment.new(content: "chiba", user_id: @user.id, stroll_id: "")
+    comment = Comment.new(content: "chiba", user_id: @second_user.id, stroll_id: "")
     expect(comment).not_to be_valid
   end
   it "コメントとuser_idとstroll_idが入力されてたらバリデーションが通る" do
-    comment = Comment.new(content: "chiba", user_id: @user.id, stroll_id: @stroll.id)
+    comment = Comment.new(content: "chiba", user_id: @second_user.id, stroll_id: @stroll.id)
     expect(comment).to be_valid
   end
 end
