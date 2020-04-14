7111431f2a983e9e4a8494f325690c6d2bd41238
daisuke_watanabe2 <daisuke_watanabe2@icloud.com>
2020-04-14 11:41:40 +0900

    Userのテストを修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,23 @@ RSpec.describe User, type: :model do
     user = User.new(name: '', email: 'failure@com', password: 'failure', password_confirmation: 'failure')
     expect(user).not_to be_valid
   end
+  it 'お名前が他のユーザーと重複するならバリデーションが通らない' do
+    name = "error"
+    user = User.create(name: name, email: 'user@com', password: 'failure', password_confirmation: 'failure')
+    second_user = User.create(name: name, email: 'second_user@com', password: 'failure', password_confirmation: 'failure')
+    expect(user).to be_valid
+    expect(second_user).not_to be_valid
+  end
   it 'メールアドレスが空ならバリデーションが通らない' do
     user = User.new(name: 'failure', email: '', password: 'failure', password_confirmation: 'failure')
     expect(user).not_to be_valid
+  end
+  it 'メールアドレスが他のユーザーと重複するならバリデーションが通らない' do
+    email = "error@com"
+    user = User.create(name: 'user', email: email, password: 'failure', password_confirmation: 'failure')
+    second_user = User.create(name: 'second_user', email: email, password: 'failure', password_confirmation: 'failure')
+    expect(user).to be_valid
+    expect(second_user).not_to be_valid
   end
   it 'パスワードが空ならバリデーションが通らない' do
     user = User.new(name: 'failure', email: 'failure@com', password: '', password_confirmation: 'failure')
