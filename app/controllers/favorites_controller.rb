ba8512a41f61361957a38f4e709cdb57cac1b514
daisuke_watanabe2 <daisuke_watanabe2@icloud.com>
2020-04-17 21:24:36 +0900

    デザインを修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,10 @@
 class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.create(stroll_id: params[:stroll_id])
-    redirect_to strolls_url, notice: "#{favorite.stroll.user.name}さんの記事をお気に入りに登録しました。"
+    redirect_to strolls_url, notice: "#{favorite.stroll.user.name}さんの『#{favorite.stroll.title}』をお気に入りに登録しました。"
   end
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to strolls_url, notice: "#{favorite.stroll.user.name}さんの記事をお気に入りから解除しました。"
+    redirect_to strolls_url, notice: "#{favorite.stroll.user.name}さんの『#{favorite.stroll.title}』をお気に入りから解除しました。"
   end
 end
