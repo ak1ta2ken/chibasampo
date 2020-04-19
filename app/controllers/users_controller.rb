0b71f64b016ee0e0bfcd941e8a6bf1e2df5f6a2e
daisuke_watanabe2 <daisuke_watanabe2@icloud.com>
2020-04-20 05:59:41 +0900

    デザインを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
-    @strolls = @user.strolls
+    @strolls = @user.strolls.order(created_at: :desc).page(params[:page])
   end
   def favorites
     @user = current_user
-    @favorites = Favorite.where(user_id: @user.id).all
+    @favorites = Favorite.where(user_id: @user.id).all.order(created_at: :desc).page(params[:page])
   end
   def new_guest
     user = User.find_or_create_by!(name: 'guest', email: 'guest@com') do |user|
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,5 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :stroll
+  paginates_per 30
 end
